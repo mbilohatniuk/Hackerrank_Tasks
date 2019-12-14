6bf290a3ecfca1b88daf5fde174ce2011de88b18
mbilohatniuk <m.bilohatniuk@gmail.com>
2019-12-14 13:06:16 +0200

    initial commit

---
+++ Hackerrank_tasks/Hackerrank_tasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hackerrank_tasks
+//
+//  Created by Maksym Bilohatniuk on 12/14/19.
+//  Copyright © 2019 Maksym Bilohatniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
